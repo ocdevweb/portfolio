243fc79791a038cecebe18aa1be328435a0b6425
Pierre-Paul TACHER <pierre-paul@Pierre-PaulnoMacBook-Pro.local>
2024-08-12 23:26:39 +0200

    first commmit.

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ window.addEventListener('load', event => {
     var form = document.getElementById("profile")
     form.addEventListener('submit', event => {
         event.preventDefault()
-        fetch("https://bitsflip.io/test", {
+        fetch("http://localhost/test", {
             method: "GET",
             headers: {
                 "Content-type": "application/json; charset=UTF-8",
